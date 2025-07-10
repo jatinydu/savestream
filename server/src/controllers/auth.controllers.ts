f825a4aabf8fa9d6a115ec73851aded5c7c71148
jatinthisside <jatinkyadu@gmail.com>
2025-07-10 20:38:57 +0530

    model: add model to store user info

--- server/src/controllers/auth.controllers.ts
+++ server/src/controllers/auth.controllers.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { ENV } from '../config';
 
 export const login=async(req:Request,res:Response)=>{
    try{
@@ -12,7 +13,7 @@ export const login=async(req:Request,res:Response)=>{
 
 export const signup=async(req:Request,res:Response)=>{
     try{
-    
+       
     }catch(error:any){
             console.error("🔴 Signup error:", error.message);
             res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({success:false, message: "Internal server error" });
---
+++ server/src/models/user.model.ts
@@ -0,0 +1,22 @@
+import { Schema, model, Document } from 'mongoose';
+
+interface IUser extends Document {
+    username: string;
+    email: string;
+    password: string;
+    is_deleted: boolean;
+    posts: string[]; 
+}
+
+const userSchema = new Schema<IUser>({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    is_deleted: { type: Boolean, default: false },
+    posts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Post'
+    }]
+},{ timestamps: { createdAt:'created_at', updatedAt:'updated_at' } });
+
+const User = model<IUser>('User', userSchema);
+export default User;
