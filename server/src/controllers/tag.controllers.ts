3bbf788300d40026578eba005cb370f4baddc50a
jatinthisside <jatinkyadu@gmail.com>
2025-07-11 12:20:46 +0530

    feat: add controller to add tags

---
+++ server/src/controllers/tag.controllers.ts
@@ -0,0 +1,61 @@
+import { Response, Request } from "express"
+import { StatusCodes } from "http-status-codes"
+import { Tag } from "../models"
+
+export const createTag = async (req: Request, res: Response) => {
+  try {
+    const { name } = req.body
+    if (!name) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ success: false, message: "Tag name is required" })
+    }
+
+    const existingTag = await Tag.findOne({ name })
+    if (existingTag) {
+      return res
+        .status(StatusCodes.CONFLICT)
+        .json({ success: false, message: "Tag already exists" })
+    }
+
+    const newTag = new Tag({ name })
+    const savedTag = await newTag.save()
+
+    res.status(StatusCodes.CREATED).json({
+      success: true,
+      message: "Tag created successfully",
+      tag: {
+        id: savedTag._id,
+        name: savedTag.name,
+        created_at: savedTag.created_at,
+        updated_at: savedTag.updated_at,
+      },
+    })
+  } catch (error: any) {
+    console.error("🔴 Error creating tag:", error.message)
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ success: false, message: "Internal Server Error" })
+  }
+}
+
+export const getTags = async (req: Request, res: Response) => {
+    try {
+        const tags = await Tag.find().sort({ created_at: -1 })
+        res.status(StatusCodes.OK).json({
+        success: true,
+        message: "Tags retrieved successfully",
+        tags: tags.map(tag => ({
+            id: tag._id,
+            name: tag.name,
+            created_at: tag.created_at,
+            updated_at: tag.updated_at,
+        })),
+        })
+    } catch (error: any) {
+        console.error("🔴 Error retrieving tags:", error.message)
+        res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ success: false, message: "Internal Server Error" })
+    }
+}
