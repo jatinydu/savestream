d14569528ca02be30e27191ced3482bd86d85dad
jatinthisside <jatinkyadu@gmail.com>
2025-07-15 10:52:40 +0530

    feat: add simple input component to lib

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,25 @@
-import CtaBtn from "./components/lib/CtaBtn"
 import { Import, Star, Plus } from 'lucide-react';
-import ActionBtn  from "./components/lib/ActionBtn";
+import {SimpleInput} from "./components/lib/Input";
 
 function App() {
   return (
     <>
-      <div className="flex flex-col gap-1.5 py-10 px-10">
-         <CtaBtn label="Save" variant="primary" size="large" startIcon={<Import size={20}/>} onClick={()=>{}}/>
-         <CtaBtn label="Save" variant="primary" size="medium" startIcon={<Import size={18}/>} onClick={()=>{}}/>
-         <CtaBtn label="Save" variant="primary" size="small" startIcon={<Import size={15}/>} onClick={()=>{}}/>
-         <br />
-          <CtaBtn label="Starred" variant="ghost" size="small" startIcon={<Star size={15}/>} onClick={()=>{}}/>
-          <CtaBtn label="Starred" variant="ghost" size="medium" startIcon={<Star size={18}/>} onClick={()=>{}}/>
-          <CtaBtn label="Starred" variant="ghost" size="large" startIcon={<Star size={20}/>} onClick={()=>{}}/>
-          <br />
-          <div className="flex gap-2">
-          <ActionBtn variant="add" size="medium" icon={<Plus size={18}/>} onClick={()=>{}}/>
-          <ActionBtn variant="add" size="large" icon={<Plus size={20}/>} onClick={()=>{}}/>
-          <ActionBtn variant="add" size="small" icon={<Plus size={16}/>} onClick={()=>{}}/>
-          <ActionBtn variant="delete" size="small" icon={<Plus size={16}/>} onClick={()=>{}}/>
-          <ActionBtn variant="ghost" size="small" icon={<Plus size={16}/>} onClick={()=>{}}/>
-          <ActionBtn variant="quick" size="xsmall" icon={<Plus size={16}/>} onClick={()=>{}}/>
-          </div>
+      <div className="flex flex-col gap-1.5 py-10 px-10 font-sans">
+         <SimpleInput
+          type="text"
+          placeholder="Simple Input"
+          className="bg-white text-gray-800"
+          size="md"
+          />
+
+          {/* <AiInput 
+          type="text"
+          placeholder="AI Input"
+          className="bg-gray-100 text-gray-800"
+          size="md"
+          startIcon={<Import className="text-gray-500" />}
+          endIcon={<Star className="text-yellow-500" />}
+          /> */}
       </div>
     </>
   )
--- client/src/components/lib/Input.tsx
+++ client/src/components/lib/Input.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+interface InputProps {
+  type?: string;
+  placeholder?: string;
+  value?: string;
+  disabled?: boolean;
+  className?: string;
+  reference?: React.Ref<HTMLInputElement>;
+  required?: boolean;
+  name?: string;
+  size?: "sm" | "md" | "lg";
+}
 
-export default function Input() {
+// interface AiProps extends InputProps {
+//   startIcon?: React.ReactNode;
+//   endIcon?: React.ReactNode;
+// }
+
+const sizeStyles = {
+  sm: "h-10 min-w-[18rem] px-2 font-xs placeholder:font-semibold placeholder:text-[13px]",
+  md: "h-11 min-w-[20rem] px-3 text-base placeholder:font-300 placeholder:text-sm",
+  lg: "h-12 min-w-[35rem] px-4 text-lg placeholder:font-300 placeholder:text-base"
+}
+
+const simple = "border-2 border-gray-200 focus:border-blue-600 focus:ring-blue-600 outline-none";
+// const ai = "border-gray-400 focus:border-blue-600 focus:ring-blue-600"
+const defaultStyles = "h-10 px-3 rounded-2xl text-black placeholder:font-300";
+
+export function SimpleInput({type,placeholder,value,reference,className,required,name,size="md"}: InputProps) {
   return (
     <div>
-      
+      <input type={type} placeholder={placeholder} value={value} ref={reference} className={`${defaultStyles} ${className} ${simple} ${sizeStyles[size]}`} required={required} name={name} />
     </div>
   )
 }
+
+// export function AiInput({type,placeholder,value,reference,className,required,name,size="md"}: AiProps) {
+//   return (
+//     <div>
+//       <input type={type} placeholder={placeholder} value={value} ref={reference} className={`${defaultStyles} ${className} ${ai} ${sizeStyles[size]}`} required={required} name={name} />
+//     </div>
+//   )
+// }
