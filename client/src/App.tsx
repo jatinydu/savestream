bd8527149fdf9dd16a970f739a9f85f7603dfbf3
jatinthisside <jatinkyadu@gmail.com>
2025-07-15 12:00:31 +0530

    feat: setup generic structure for Toast component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,10 @@
-import { Import, Star, Plus } from 'lucide-react';
-import {SimpleInput, AiInput} from "./components/lib/Input";
-import { PiShootingStarLight } from "react-icons/pi";
-import { FiSearch } from "react-icons/fi";
-
+import Toast from "./components/lib/Toast"
 
 function App() {
   return (
-    <>
-      <div className="flex flex-col gap-1.5 py-10 px-10 font-sans">
-         <SimpleInput
-          type="text"
-          placeholder="Simple Input"
-          className="bg-white text-gray-800"
-          size="md"
-          />
-
-          <AiInput 
-          type="text"
-          placeholder="Ask your brain anything..."
-          startIcon={<PiShootingStarLight color='blue'/>}
-          endIcon={<FiSearch color="white" size={13}/>}
-          />
-      </div>
-    </>
+    <div className="w-screen h-screen flex justify-center items-center">
+         <Toast message="successfull transaction" variant="success" />
+    </div>
   )
 }
 
--- client/src/components/lib/Toast.tsx
+++ client/src/components/lib/Toast.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import ActionBtn from "./ActionBtn";
+import { GrClose } from "react-icons/gr";
+import { VscCheck } from "react-icons/vsc";
+import { MdOutlineErrorOutline } from "react-icons/md";
+import { TbInfoTriangle } from "react-icons/tb";
+import { RiAlarmWarningLine } from "react-icons/ri";
 
-export default function Toast() {
+interface ToastProps {
+  message: string;
+  duration?: number; 
+  onClose?: () => void; 
+  variant?: 'success' | 'error' | 'info' | 'warning'; 
+}
+
+const variantTitle = {
+  success: 'Sucesss!',
+  error: 'Failiure!',
+  info: 'Info!',
+  warning: 'Warning!',
+}
+
+const variantIcon = {
+  success: <span className="text-green-500 px-1 py-1 border-2 border-green-500 rounded-full font-bold text-md"><VscCheck/></span>,
+  error: <span className="text-red-500 px-1 py-1 border-2 border-red-500 rounded-full font-bold text-md"><MdOutlineErrorOutline/></span>,
+  info: <span className="text-blue-500 px-1 py-1 border-2 border-blue-500 rounded-full font-bold text-md"><TbInfoTriangle/></span>,
+  warning: <span className="text-yellow-500 px-1 py-1 border-2 border-yellow-500 rounded-full font-bold text-md"><RiAlarmWarningLine/></span>,
+}
+
+const variantStyle={
+  success: 'bg-green-100 text-green-800',
+  error: 'bg-red-100 text-red-800',
+  info: 'bg-blue-100 text-blue-800',
+  warning: 'bg-yellow-100 text-yellow-800',
+}
+
+const defaultStyle = 'flex flex-col justify-between items-start p-4 rounded-lg shadow-md w-80 gap-2';
+
+export default function Toast({message="Test message..",variant="success"}: ToastProps) {
   return (
-    <div>
-      
+    <div className={`${variantStyle[variant]} ${defaultStyle}`}>
+      <span className="flex gap-1.5 items-center">
+        {variantIcon[variant]} {variantTitle[variant]}
+      </span>
+      <p>{message}</p>
+      <ActionBtn variant="ghost" size="small" icon={<GrClose/>} onClick={() => {}} className="" />
     </div>
   )
 }
