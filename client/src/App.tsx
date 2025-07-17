f950d314d02aa3e1d6acbe2fb01b10c472028ba5
jatinthisside <jatinkyadu@gmail.com>
2025-07-17 20:22:39 +0530

    feat: add toggle component to lib

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,9 @@
-import useToast from "./hooks/useToast";
-import CtaBtn from "./components/lib/CtaBtn"
+import Toggle from "./components/lib/Toggle";
 
 function App() {
-  const { showToast } = useToast();
-  const submitHandler = () => {
-    console.log("Button clicked!");
-    showToast({
-      message: "This is a toast message!",
-      variant: "success",
-      position: "bottom-right",
-      duration: 9000
-    })
-  }
-  const errorHandler = () => {
-    console.log("Error button clicked!");
-    showToast({
-      message: "This is an error message!",
-      variant: "error",
-      position: "bottom-right",
-      duration: 3000
-    })
-  }
   return (
     <div className="w-screen h-screen flex justify-center items-center pt-10">
-         <CtaBtn variant="primary" size="medium" label="Click me" onClick={submitHandler}/>
-         <CtaBtn variant="ghost" size="medium" label="Click me" onClick={errorHandler}/>
+         <Toggle/>
     </div>
   )
 }
--- client/src/components/lib/Toggle.tsx
+++ client/src/components/lib/Toggle.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import { useState } from "react";
+import CtaBtn from "./CtaBtn";
+import { Share2 } from "lucide-react";
 
-export default function Toggle() {
+const Toggle = () => {
+  const [checked, setChecked] = useState(false);
+  const toggle=()=>{
+    setChecked(!checked);
+  }
   return (
-    <div>
-      
+    <div className="flex items-center space-x-3">  
+      {
+        checked && <CtaBtn 
+        variant="ghost"
+        size="medium"
+        startIcon={<Share2 size={15}/>}
+        className=""
+        label="Share Brain"
+        onClick={() => console.log("Share Brain clicked")}
+      />  
+      }
+      <button
+        type="button"
+        role="switch"
+        aria-checked={checked}
+        onClick={toggle}
+        className={`relative inline-flex h-7 w-13 items-center rounded-full transition-colors ${
+          checked ? 'bg-primary' : 'bg-gray-200'
+        }`}
+      >
+        <span
+          className={`inline-block h-5 w-6 transform rounded-full bg-white transition-transform ${
+            checked ? 'translate-x-6' : 'translate-x-1'
+          }`}
+        />
+      </button>
+      <span className="text-sm font-medium">
+        {
+          checked ? 'Public' : 'Private'
+        }
+      </span>
     </div>
-  )
-}
+  );
+};
+
+export default Toggle;
