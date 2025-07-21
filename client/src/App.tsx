42f9dd03946265309c543a58bb7ec4494e18de6f
jatinthisside <jatinkyadu@gmail.com>
2025-07-21 19:46:00 +0530

    feat: add ui for suggestions dialog of autocomplete input

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import AddPostModel from "./components/createPost/AddPostModel";
 
 function App() {
   return (
-    <div className="w-screen h-screen flex justify-center items-center pt-10 bg-gray-300">
+    <div className="w-screen h-screen flex justify-center items-center pt-10 bg-gray-300 overflow-x-hidden">
         <AddPostModel/>
     </div>
   )
--- client/src/components/autoComplete/Suggestions.tsx
+++ client/src/components/autoComplete/Suggestions.tsx
@@ -1,13 +1,10 @@
-import React from 'react'
-
 export default function Suggestions(props:{ categories: any[] }) {
   return (
-    <div className='w-full flex flex-col gap-1 max-h-[100px] overflow-x-hidden overflow-y-scroll scroll-auto'>
+    <div className='w-full flex flex-col gap-1 max-h-[100px] overflow-y-scroll bg-gray-200 py-1 rounded-lg custom-scrollbar'>
       {
         props.categories.map((category, index) => (
-          <div key={index} className='flex items-center justify-between px-3 py-2 rounded-lg hover:bg-gray-100 cursor-pointer'>
-            <span className='text-gray-700'>{category.title}</span>
-            <span className='text-gray-500 text-sm'>{category.category}</span>
+          <div key={index} className='flex items-center justify-between px-3 py-2 rounded-lg hover:bg-gray-300 cursor-pointer transition-all duration-300'>
+            <span className='text-gray-700 hover:text-gray-600'>{category.title}</span>
           </div>
         ))
       }
