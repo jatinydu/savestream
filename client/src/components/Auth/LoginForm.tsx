b21cfa36f352c7d3586ea55bd75a7ada031df44f
jatinthisside <jatinkyadu@gmail.com>
2025-07-22 15:27:30 +0530

    fix: made login page responsive

--- client/src/components/Auth/LoginForm.tsx
+++ client/src/components/Auth/LoginForm.tsx
@@ -6,7 +6,7 @@ import CtaBtn from '../lib/CtaBtn';
 
 export default function LoginForm(props:AuthCommonProps) {
   return (
-    <div className='lg:w-[28vw] flex flex-col p-4 px-8 border-2 border-gray-200 py-6 mt-6 rounded-2xl'>
+    <div className='lg:w-[28vw] md:w-[50vw] w-[90vw] flex flex-col p-4 px-8 border-2 border-gray-200 py-6 mt-6 rounded-2xl'>
        <h4 className='font-serif font-semibold text-2xl mb-3'>Signin to your account</h4>
        <form className='flex flex-col gap-6 mt-3 w-full'>
           <CreateInput startIcon={<Mail size={15} color="#99a1af"/>} required={true} type="email" label='Email' placeholder='Enter your email' tagType='input' className='h-[2.5rem] focus-within:ring-2 focus-within:ring-primary rounded-sm border-2 border-gray-100'/>
