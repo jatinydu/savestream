4c272e87ca625524a76e8e7f2b40e16a367fb4f6
jatinthisside <jatinkyadu@gmail.com>
2025-07-22 16:10:08 +0530

    fix: add error toast to ProtectedRoute

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,17 @@
+import PrivateRoutes from "./hooks/usePrivateRoutes"
 import AuthForm from "./pages/AuthForm"
+import { Route, Routes } from "react-router"
 
 function App() {
   return (
-    <div className="w-screen h-screen flex justify-center items-center pt-10 overflow-x-hidden">
-        <AuthForm type="signup" title="Create your account" desc="Start building your knowledge base"/>
+    <div className="w-screen h-screen flex justify-center items-center pt-10 overflow-x-hidden z-30">
+        <Routes>
+          <Route element={<PrivateRoutes/>}>
+             <Route path="/feed" element={<h1>Home Page</h1>} />
+          </Route>
+          <Route path="/" element={<AuthForm type="login" title="Welcome back!" desc="Login to your account"/>} />
+          <Route path="/signup" element={<AuthForm type="signup" title="Create a free account" desc="Join us today!"/>} />
+        </Routes>
     </div>
   )
 }
--- client/src/hooks/usePrivateRoutes.tsx
+++ client/src/hooks/usePrivateRoutes.tsx
@@ -1,8 +1,30 @@
 import { Navigate, Outlet } from 'react-router'
+import useToast from './useToast'
+import { useCallback, useEffect } from 'react'
 
 const PrivateRoutes = () => {
-  let auth = {'token':true}
+  let auth = {'token':false}
+  const {showToast} = useToast()
+  const displayToast = useCallback(()=>{
+    if(!auth.token){
+        showToast({
+            message: 'You need to login first',
+            variant: 'error',
+            position: 'bottom-right',
+            duration: 4000
+        })
+      }
+  },[])
+ 
+  useEffect(()=>{
+    setTimeout(() => {
+        displayToast();
+    }, 1000);
+  },[])
+
 return (
     auth.token ? <Outlet/> : <Navigate to='/'/>
   )
 }
+
+export default PrivateRoutes
