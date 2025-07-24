f9b49bb9b51078556e93950fd47e8d61d3b936b7
jatinthisside <jatinkyadu@gmail.com>
2025-07-24 19:37:40 +0530

    fix: bug on generateAuthToken utility

--- server/src/utils/auth.ts
+++ server/src/utils/auth.ts
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 import { ENV } from '../config';
 
 export const generateAuthToken=(data:any)=>{
-    const token = jwt.sign({}, ENV.JWT_SECRET as string, {
+    const token = jwt.sign(data, ENV.JWT_SECRET as string, {
         expiresIn: '1d'
     });
     return token;
